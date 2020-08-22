97a0bef75af5eaae08a8c9a3fd033dd0573331fa
Abhinav <abhinav16032002@gmail.com>
2020-08-22 15:57:30 +0000

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 var express = require("express");
 var router = express.Router();
 
-
+//home page route
 router.get('/', function(req, res){
    res.render("landing"); 
 });
 
+//about page route
 router.get("/about", function(req, res){
    res.render("about"); 
+});
+
+//contact page route
+router.get("/contact", function(req, res){
+   res.render("contact");
+});
+
+//policy page route
+router.get("/policy/show", function(req, res){
+   res.render("policy/show")
+});
+
+router.get("/provider/show", function(req, res){
+   res.render("provider/show")
+});
+
+router.get("/seeker/show", function(req, res){
+   res.render("seeker/show")
 });
 module.exports = router;
