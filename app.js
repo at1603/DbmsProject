a284a2d893844237c03ba0e4285d63a0b664b829
Abhinav <abhinav16032002@gmail.com>
2020-08-24 15:47:21 +0000

    added packages

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    path  = require("path"),
+    mongoose = require("mongoose");
 
-var bodyParser = require("body-parser");
-var path  = require("path");
+var Employee = require("./models/employeeSchema"),
+    Employer = require("./models/employerSchema"),
+    Blog = require("./models/blogSchema");
+    
+var indexRoutes = require('./routes/index'),
+    authRoutes = require("./routes/authRoutes");
 
-app.use(express.static(path.join(__dirname + '/public')));
-var indexRoutes = require('./routes/index');
-var authRoutes = require("./routes/authRoutes");
-
+mongoose.connect("mongodb://localhost/jobpost", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname + '/public')));
+
+//Passport Configuration
+// app.use(require("express-session")({
+//     secret: "This is my secret",
+//     resave: false,
+//     saveUninitialized: false
+// }));
+
 app.use(indexRoutes);
 app.use(authRoutes);
 
