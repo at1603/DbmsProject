dbb9407f4e229955730669f9cda1abcbede905bf
Abhinav <abhinav16032002@gmail.com>
2020-08-25 20:35:48 +0000

    added passport functions

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ var express = require("express"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
     path  = require("path"),
-    mongoose = require("mongoose"),
-    Blog = require("./models/blogSchema");
+    mongoose = require("mongoose");
 
-var Employee = require("./models/employeeSchema"),
-    Employer = require("./models/employerSchema"),
+var User = require("./models/userSchema"),
     Blog = require("./models/blogSchema");
     
 var indexRoutes = require('./routes/index'),
@@ -21,11 +19,25 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + '/public')));
 
 //Passport Configuration
-// app.use(require("express-session")({
-//     secret: "This is my secret",
-//     resave: false,
-//     saveUninitialized: false
-// }));
+app.use(require("express-session")({
+    secret: "This is my secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    console.log(req.user);
+   res.locals.currentUser = req.user;
+   next();
+});
 
 app.use(indexRoutes);
 app.use(authRoutes);
