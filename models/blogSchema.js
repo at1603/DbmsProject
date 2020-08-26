7a37f27b09e0aa3abd5e24e2baf9c6ca430ef397
Abhinav <abhinav16032002@gmail.com>
2020-08-26 19:34:45 +0000

    modified blog schema

--- models/blogSchema.js
+++ models/blogSchema.js
@@ -3,7 +3,16 @@ var mongoose = require("mongoose");
 var blogSchema = new mongoose.Schema({
    title:String,
    image:String,
-   description:String
+   description:String,
+   genre:String,
+   createdAt:{type:Date, default:Date.now},
+   author:{
+      id:{
+         type:mongoose.Schema.Types.ObjectId,
+         ref:"User"
+      },
+      username:String
+   }
 });
 
 module.exports = mongoose.model("Blog", blogSchema);
