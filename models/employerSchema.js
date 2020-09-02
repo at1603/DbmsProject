549881f37be0325f1f8d3021e56d80e309ba2e3f
Abhinav <abhinav16032002@gmail.com>
2020-09-02 20:06:22 +0000

    added some fields

---
+++ models/employerSchema.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+
+var employerSchema = new mongoose.Schema({
+    companyName:String,
+    companyAddress:{
+        streetAddress:String,
+        city:String,
+        state:String,
+        zip:String,
+    },
+    designation:String,
+    grad:String,
+    gradCol:String,
+    pgrad:String,
+    pgradCol:String,
+    description:String,
+    user:{
+      id:{
+         type:mongoose.Schema.Types.ObjectId,
+         ref:"User"
+      },
+      username:String
+   }
+});
+
+module.exports = mongoose.model("Employer", employerSchema);
