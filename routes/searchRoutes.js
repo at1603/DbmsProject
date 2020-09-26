e09e7d2bbc8aed4f2b82a17bfdfbba972f93d6e0
Abhinav <abhinav16032002@gmail.com>
2020-09-26 16:50:39 +0000

    added routes

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -31,4 +31,57 @@ router.post("/searchPage/:id",middleware.isLoggedIn, function(req,res){
     });
 });
 
+//offer Page
+router.get("/offerPage/:id", middleware.isLoggedIn, function(req,res){
+    User.findById(req.params.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            Employer.find().where('handler.id').equals(foundUser._id).exec(function(err, foundEmp){
+               if(err){
+                   console.log(err);
+               }else{
+                    res.render("search/offerPage", {user:foundUser, emp: foundEmp}); 
+               }
+            });
+        }
+    })
+});
+
+router.post("/offerPage/:id",middleware.isLoggedIn, function(req,res){
+    var handler = {
+              id: req.user._id,
+              username: req.user.username
+        },
+        loc = req.body.location,
+        sal = req.body.salary,
+        vac = req.body.vacancy, 
+        ToJob = req.body.ToJob,
+        skill = req.body.skill,
+        qual = req.body.qual,
+        lastApply = req.body.lastApply,
+        contact = req.body.contact,
+        mail = req.body.mail;
+        
+    var newJob = {handler: handler,
+                    location: loc,
+                    vacancy:vac,
+                    ToJob: ToJob,
+                    skill: skill,
+                    qual: qual,
+                    lastApply: lastApply,
+                    contact:contact,
+                    mail: mail
+    }
+    
+    Job.create(newJob, function(err, nJob){
+       if(err){
+           console.log(err);
+       } else{
+           res.redirect("/");
+       }
+    });
+})
+
+
 module.exports = router;
