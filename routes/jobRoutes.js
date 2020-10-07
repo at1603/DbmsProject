ed4b33ec68c9d51e20f34f2156557b212ac859c7
Abhinav <abhinav16032002@gmail.com>
2020-10-08 00:32:20 +0530

    added jobRoute file

---
+++ routes/jobRoutes.js
@@ -0,0 +1,46 @@
+var express = require("express"),
+    router = express.Router(),
+    User = require("../models/userSchema"),
+    Blog = require("../models/blogSchema"),
+    Employer = require("../models/employerSchema"),
+    Resume = require('../models/resumeSchema'),
+    Job = require("../models/jobSchema"),
+    middleware = require('../middleware');
+
+
+
+//job edit route
+router.get("/jobs/:id/edit", function(req,res){
+    Job.findById(req.params.id, function(err, foundJob){
+        if(err){
+            res.redirect("/");
+        }else{
+            res.render("provider/jobEdit", {job:foundJob});  
+        }
+    });
+});
+
+//update route
+router.put("/jobs/:id", function(req,res){
+    Job.findByIdAndUpdate(req.params.id, req.body.job, function(err, upadateJob){
+       if(err){
+           res.redirect("/");
+       }else{
+           res.redirect("/");
+       }
+    });
+ });
+
+
+//job delete route
+router.delete("/jobs/:id", function(req, res){
+    Job.findByIdAndRemove(req.params.id, function(err,){
+        if(err){
+            res.redirect("/");
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+module.exports = router;
