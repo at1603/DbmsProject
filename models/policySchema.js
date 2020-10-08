72fd6015c890fc9ec6e5c0a27e74c72134d2351d
Abhinav <abhinav16032002@gmail.com>
2020-10-08 20:49:51 +0530

    updated policy schema

--- models/policySchema.js
+++ models/policySchema.js
@@ -1,10 +1,10 @@
-var mongoose = require("monogose");
+var mongoose = require("mongoose");
 
 var policySchema = new mongoose.Schema({
     relTo: String,
     polHead: String,
     polDesc: String,
-    polRef: [{type:String}]
+    createdAt:{type:Date, default:Date.now},
 });
 
 module.exports = mongoose.model("Policy", policySchema);
