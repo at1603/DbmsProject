fef39e6c487dd9e4cca3ac75c48b57c884ebfe47
Abhinav <abhinav16032002@gmail.com>
2020-10-24 22:55:02 +0530

    added flash messages

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -7,6 +7,7 @@ var Blog = require("../models/blogSchema"),
 router.get("/blogs", function(req, res){
     Blog.find({}, function(err, allBlogs){
         if(err){
+            req.flash("error", "Something went wrong");
             console.log(err);
         }else{
             res.render("blog/index", {blogs:allBlogs}); 
@@ -27,8 +28,10 @@ router.post("/blogs", middleware.isLoggedIn, function(req,res){
     var newBlog = {title:title, image: image, description:desc, author:author,genre:genre};
     Blog.create(newBlog, function(err, newlyCreated){
       if(err){
+          req.flash("Something went wrong! ")
           console.log(err);
       }else{
+            req.flash("success", "New Blog Posted!")
             res.redirect("/blogs");
       }
     });
@@ -41,7 +44,7 @@ router.get("/blogs/new", middleware.isLoggedIn, function(req, res){
 router.get("/blogs/:id", function(req, res){
   Blog.findById(req.params.id, function(err, foundBlog){
       if(err){
-          console.log(err);
+        req.flash("error", "Blog not found!")
       } else{
           res.render("blog/show", {blog:foundBlog});
       }
@@ -52,6 +55,7 @@ router.get("/blogs/:id", function(req, res){
 router.get("/blogs/:id/edit", middleware.checkBlogOwnership, function(req,res){
     Blog.findById(req.params.id, function(err, foundBlog){
         if(err){
+            req.flash("error", "Blog not found!")
             res.redirect("blogs/edit");
         }else{
             res.render("blog/edit", {blog:foundBlog});  
@@ -65,6 +69,7 @@ router.put("/blogs/:id", middleware.checkBlogOwnership, function(req,res){
        if(err){
            res.redirect("/blogs");
        }else{
+           req.flash("success", "Blog Successfully Updated");
            res.redirect("/blogs/"+req.params.id);
        }
     });
@@ -74,9 +79,10 @@ router.put("/blogs/:id", middleware.checkBlogOwnership, function(req,res){
 router.delete("/blogs/:id", middleware.checkBlogOwnership, function(req, res){
     Blog.findByIdAndRemove(req.params.id, function(err,){
         if(err){
+            req.flash("error", "Blog Not Found! ")
             res.redirect("/blogs");
         }else{
-            // req.flash("success", "Campground Successfully Deleted");
+            req.flash("success", "Blog Successfully Deleted");
             res.redirect("/blogs");
         }
     });
